1db6943b32b6f89ca41a81551a9c19eca045703b
VladiusVostokus <kovalenkov222@gmail.com>
2025-01-06 12:09:02 +0200

    Add descriptor interface

--- FileSystem/descriptor.go
+++ FileSystem/descriptor.go
@@ -1,8 +1,18 @@
 package filesystem
 
+type Descriptor interface {
+	Init(fileType string, id int)
+}
+
 type fileDescriptor struct{
 	FileType string
 	Nlink, NOpen, Size, Id int
 	Data map[int]*Block
 	Nblock int
+}
+
+func (fd *fileDescriptor) Init (fileType string, id int) {
+	fd.FileType = "reg"
+	fd.Id = id
+	fd.Nlink = 1
 }
--- FileSystem/fs.go
+++ FileSystem/fs.go
@@ -6,18 +6,20 @@ import (
 )
 
 type FileSystem struct {
-	directory map[string]*fileDescriptor
+	directory map[string]Descriptor
 }
 
 func (fs * FileSystem) Mkfs () {
 	fmt.Println("Creation of file system...")
-	fs.directory = make(map[string]*fileDescriptor)
+	fs.directory = make(map[string]Descriptor)
 	fmt.Println("File system created")
 }
 
 func (fs* FileSystem) Create (fileName string) {
 	id := int(time.Now().UnixNano())
-	descriptor := &fileDescriptor{ FileType:"reg", Nlink: 1, Size: 0, Id: id, NOpen: 0 }
+	descriptor := &fileDescriptor{}
+	descriptor.Init("reg", id)
+
 	fs.directory[fileName] = descriptor
 	descriptor.Data = make(map[int]*Block)
 	fmt.Println("Create file:", fileName,"| Descriptor id:", descriptor.Id)
@@ -26,21 +28,24 @@ func (fs* FileSystem) Create (fileName string) {
 func (fs *FileSystem) Ls() {
 	fmt.Println("Hard links of currect directory:")
 	for f, d := range fs.directory {
-		fmt.Println("Name:",f ,"\t id:",d.Id)
+		if d, isFile := d.(*fileDescriptor); isFile {
+			fmt.Println("Name:",f ,"\t id:",d.Id)
+		}
 	}
 }
 
 func (fs *FileSystem) Stat(fileName string) {
-	descriptor := fs.directory[fileName]
+	descriptor := fs.directory[fileName].(*fileDescriptor)
 	fmt.Println("Type:", descriptor.FileType, 
 				"\tId:",descriptor.Id, 
 				"\tHard links count:", descriptor.Nlink, 
 				"\tSize:", descriptor.Size,
 				"\tBlocks:", descriptor.Nblock)
+	
 }
 
 func (fs *FileSystem) Link(linkWith, toLink string) {
-	descriptor := fs.directory[linkWith]
+	descriptor := fs.directory[linkWith].(*fileDescriptor)
 	descriptor.Nlink++
 	fs.directory[toLink] = descriptor
 	fmt.Println("Create hard link", toLink, "with", linkWith)
@@ -48,7 +53,7 @@ func (fs *FileSystem) Link(linkWith, toLink string) {
 
 func (fs *FileSystem) Unlink(fileName string) {
 	fmt.Println("Delete file:", fileName)
-	descriptor := fs.directory[fileName]
+	descriptor := fs.directory[fileName].(*fileDescriptor)
 	descriptor.Nlink--
 	delete(fs.directory, fileName)
 }
@@ -58,7 +63,7 @@ func (fs *FileSystem) Find(fileName string) bool {
 }
 
 func (fs *FileSystem) GetDescriptor(fileName string) *fileDescriptor {
-	return fs.directory[fileName]
+	return fs.directory[fileName].(*fileDescriptor)
 }
 
 func (fs *FileSystem) NullifyDescriptor(fileName string) {
